7735ed86d7e8e13e8e502137d40c795157c3fc52
Vinicius Benedito <beneditoloura@gmail.com>
2020-11-09 07:21:24 -0300

    Ajustes no carrinho de compra Mobile

--- script.js
+++ script.js
@@ -104,12 +104,32 @@ c('.pizzaInfo--addButton').addEventListener('click', () => {
     closeModal();
 });
 
+c('.menu-openner').addEventListener('click', () => {
+    if(cart.length > 0) {
+        c('aside').style.left = '0';
+    }
+});
+
+c('.menu-closer').addEventListener('click', () => {
+    c('aside').style.left = '100vw';
+});
+
 function updateCard() {
+    c('.menu-openner span').innerHTML = cart.length;
+
     if(cart.length > 0) {
         c('aside').classList.add('show');
         c('.cart').innerHTML = '';
+        
+        let subTotal = 0;
+        let desconto = 0;
+        let total = 0;
+
         for(let i in cart) {
             let pizzaItem = pizzaJson.find((item) => item.id == cart[i].id);
+
+            subTotal += pizzaItem.price * cart[i].qt;
+
             let cartItem = c('.models .cart--item').cloneNode(true);
 
             let pizzaSizeName;
@@ -133,12 +153,32 @@ function updateCard() {
             cartItem.querySelector('img').src = pizzaItem.img;
             cartItem.querySelector('.cart--item-nome').innerHTML = pizzaName;
             cartItem.querySelector('.cart--item--qt').innerHTML = cart[i].qt;
+            cartItem.querySelector('.cart--item-qtmenos').addEventListener('click', () => {
+                if(cart[i].qt > 1) {
+                    cart[i].qt--;
+                } else {
+                    cart.splice(i, 1);
+                }
+                updateCard();
+            });
 
+            cartItem.querySelector('.cart--item-qtmais').addEventListener('click', () => {
+                cart[i].qt++;
+                updateCard();
+            });
 
             c('.cart').append(cartItem);
         }
 
+        desconto = subTotal * 0.1;
+        total = subTotal - desconto;
+
+        c('.subtotal span:last-child').innerHTML = `R$ ${subTotal.toFixed(2)}`;
+        c('.desconto span:last-child').innerHTML = `R$ ${desconto.toFixed(2)}`;
+        c('.total span:last-child').innerHTML = `R$ ${total.toFixed(2)}`;
+
     } else {
         c('aside').classList.remove('show');
+        c('aside').style.left = '100vw';
     }
 }
